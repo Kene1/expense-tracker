ccbd6956d8bb3779332bba080cd166cbda98c4ec
keneilwe thobega <thobegak@gmail.com>
2023-07-02 03:42:12 +0200

    Updates

---
+++ precache-manifest.b3d0230516785ab3f817937fd2ebc665.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c529329e0740803fba08",
+    "url": "/expense-tracker/static/css/main.2fd0a2d7.chunk.css"
+  },
+  {
+    "revision": "c529329e0740803fba08",
+    "url": "/expense-tracker/static/js/main.c529329e.chunk.js"
+  },
+  {
+    "revision": "ca069d8683a04ec5eb78",
+    "url": "/expense-tracker/static/js/1.ca069d86.chunk.js"
+  },
+  {
+    "revision": "2c594b5b6bfedf8ec159",
+    "url": "/expense-tracker/static/js/runtime~main.2c594b5b.js"
+  },
+  {
+    "revision": "30e76ebb8f0f34f29d9cc8a99434c037",
+    "url": "/expense-tracker/index.html"
+  }
+];
